3f8868dca897f4cc14047ba6997e90ea2804a4e6
Hector Matus <hector@koibanx.com>
2023-06-23 09:20:05 -0600

    Added responsive navbar

--- src/components/elements/header/Header.tsx
+++ src/components/elements/header/Header.tsx
@@ -1,23 +1,33 @@
 import Link from 'next/link';
 import styles from './Header.module.scss';
+import { useState } from 'react';
 export default function Nav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
-    <nav>
-      <ul>
+    <nav className={styles.navbar}>
+      <label
+        htmlFor="menu-toggle"
+        className={styles['menu-icon']}
+        onClick={toggleMenu}
+      >
+        &#9776;
+      </label>
+      <ul className={`${styles.menu} ${menuOpen ? styles['show-menu'] : ''}`}>
         <li>
-          <Link className={styles.link} href="/">
-            Inicio
-          </Link>
+          <Link href="/">Home</Link>
         </li>
         <li>
-          <Link className={styles.link} href="/about">
-            Sobre nosotros
-          </Link>
+          <Link href="/about">About</Link>
         </li>
         <li>
-          <Link className={styles.link} href="#">
-            Contacto
-          </Link>
+          <Link href="/services">Services</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
         </li>
       </ul>
     </nav>
